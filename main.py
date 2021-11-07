de964a1c1428ec7a2067465c3e25f7661142f7db
chan <chan9480@naver.com>
2021-11-07 18:15:10 +0900

    211107 train_function train 함수 테스트해보기

---
+++ main.py
@@ -0,0 +1,5 @@
+from train_function import train
+
+if __name__ == "__main__":
+    train()
+    
--- train_function.py
+++ train_function.py
@@ -1,62 +1,137 @@
+import tensorflow as tf
+import os
+
+# 모델 정의 (함수형이든 sequential 이든 맘대루)
+''' 함수형'''
+input_ = tf.keras.layers.Input(shape=(28, 28, 1))
+x = tf.keras.layers.Conv2D(64, 3, activation='relu')(input_)
+x = tf.keras.layers.Flatten()(x)
+x = tf.keras.layers.Dense(128, activation='relu')(x)
+output_ = tf.keras.layers.Dense(10, activation='softmax')(x)
+
+model = tf.keras.models.Model(input_, output_)
+'''sequential 1'''
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(2, activation="relu"))
+model.add(tf.keras.layers.Dense(3, activation="relu"))
+model.add(tf.keras.layers.Dense(4))
+
+'''sequential 2'''
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Dense(2, activation="relu"),
+        tf.keras.layers.Dense(3, activation="relu"),
+        tf.keras.layers.Dense(4),
+    ]
+)
+
+#loss function 정의 (예시 sparse categorical crossentropy)
+loss_function = tf.keras.losses.SparseCategoricalCrossentropy()
+
+# optimizer 정의 (예시 Adam)
+optimizer = tf.keras.optimizers.Adam()
+
+# metric 정의
+train_loss = tf.keras.metrics.Mean()
+train_acc = tf.keras.metrics.SparseCategoricalAccuracy()
+
+val_loss = tf.keras.metrics.Mean()
+val_acc = tf.keras.metrics.SparseCategoricalAccuracy()
+
+# train_step 정의
+@tf.function
+def train_step(x, y):
+    '''
+    배치 한번에 대한 학습과정을 커스터마이징
+    '''
+    with tf.GradientTape() as tape:
+        # 1. 모델사용 예측 (prediction)
+        predictions = model(x)
+        # 2. Loss 계산
+        loss = loss_function(y, predictions)
+    
+    # 3. 그라디언트(gradients) 계산
+    gradients = tape.gradient(loss, model.trainable_variables)
+    
+    # 4. 오차역전파(Backpropagation) - weight 업데이트
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    
+    # loss와 accuracy를 업데이트 합니다.
+    train_loss(loss)
+    train_acc(y, predictions)
+
+#validation_step 정의
+@tf.function
+def val_step(x, y):
+    '''
+    1 epoch가 끝나면 test step으로 loss를 계산한다.
+    '''
+    # 1. 예측 (prediction)
+    predictions = model(x)
+    # 2. Loss 계산
+    loss = loss_function(y, predictions)
+    
+    # Val셋에 대해서는 gradient를 계산 및 backpropagation 하지 않습니다.
+    
+    # loss와 accuracy를 업데이트 합니다.
+    val_loss(loss)
+    val_acc(y, predictions)
+
+
 # 모델 체크포인트 저장
 checkpoint_dir = './training_checkpoints'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                discriminator_optimizer=discriminator_optimizer,
-                                generator=generator,
-                                discriminator=discriminator)
-
-@tf.function
-def train_step(images):
-  '''
-  배치 한번당 일어나는 학습
-  '''
-  noise = tf.random.normal([BATCH_SIZE, noise_dim])
-
-  with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-    #모델 사용
-    generated_images = generator(noise, training=True)
-    #loss계산에 필요한 값
-    real_output = discriminator(images, training=True)
-    fake_output = discriminator(generated_images, training=True)
-    #loss 계산
-    gen_loss = generator_loss(fake_output)
-    disc_loss = discriminator_loss(real_output, fake_output)
-  # gradients 계산
-  gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-  gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
-  #오치 역전파, 가중치 업데이트
-  generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-  discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
-
-
-
-def train(dataset, epochs):
+checkpoint = tf.train.Checkpoint(optimizer=optimizer,
+                                model=model)
+'''============================================================================================'''
+# train 정의
+def train(dataset, epochs, patience = 5, best_loss=1e10, n=1):
   '''
   input :
-  dataset은 tf의 데이터셋 객체여야하며, epochs는 전체데이터 학습횟수
+    dataset은 tf의 데이터셋 객체여야하며,
+    epochs는 전체데이터 학습횟수
+    patience 는 early stopping을 판단할때, 연속적으로 성능향상이 일어나지 않은 횟수가 patience값이 되면 break.
+    best_loss 값보다 val_loss가 작으면 계속해서 val_loss로 업데이트
+    n 번마다 학습결과를 print함
   
-  train_step이라는 함수로 1배치당 학습을 따로 정의하여 
-  이 함수에서 실행시킨다.
+  설명 :
+  이 함수에서 train_step, test_step 등 실행시킨다.
+  또한 특정 epoch마다 수행할 일을 정의할 수 있다.
+  early stopping을 구현해놓음.
   '''
   for epoch in range(epochs):
-    start = time.time() #start는 1 epochs마다 걸린시간(맨처음부터 누적된 시간)을 담는다.
+    #start는 1 epochs마다 걸린시간(맨처음부터 누적된 시간)을 담는다.
+    start = time.time() 
 
-    for batch in dataset:
-      train_step(batch)
-
-    # GIF를 위한 이미지를 바로 생성합니다.
-    display.clear_output(wait=True)
-    generate_and_save_images(generator, epoch + 1, seed)
-
+    # 1batch씩 train함.
+    for batch_x, batch_y in dataset: 
+      train_step(batch_x, batch_y)
+    
+    # test셋에 대해서 test함
+    for val_x, val_y in validation_data:
+      val_step(val_x, val_y)
+    
+    # n에포크 마다 결과들을 print한다.
+    if (epoch + 1) % n == 0:
+      template = '에포크: {}, 손실: {:.5f}, 정확도: {:.2f}%, 검증 손실: {:.5f}, 검증 정확도: {:.2f}%'
+      print (template.format(epoch+1,
+                            train_loss.result(),
+                            train_acc.result()*100,
+                            val_loss.result(),
+                            val_acc.result()*100))
+      print ('소요시간 {} sec'.format(time.time()-start))
     # 15 에포크가 지날 때마다 모델을 저장합니다.
     if (epoch + 1) % 15 == 0:
       checkpoint.save(file_prefix = checkpoint_prefix)
-    
-    # print (' 에포크 {} 에서 걸린 시간은 {} 초 입니다'.format(epoch +1, time.time()-start))
-    print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
 
-  # 마지막 에포크가 끝난 후 생성합니다.
-  display.clear_output(wait=True)
-  generate_and_save_images(generator, epochs, seed)
+    # early stopping 구현. (예시: epoch연속으로 patience 수만큼 val_loss감소가 없다면 STOP!!)
+    wait += 1
+    if val_loss > best:
+      best = val_loss
+      wait = 0
+    if wait >= patience:
+      break
+  # 마지막 에포크가 끝난 후 할일 이 아래에 작성
+  '''pass'''
 
+'''-------------------------------------------------------------------------------------------'''
